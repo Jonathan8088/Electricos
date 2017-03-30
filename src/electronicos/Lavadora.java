f64087bd2c48d18b6e8e895ec6aed500ec921e77
Jonathan <Jonathan@Jonathan-PC>
2017-03-30 17:24:10 -0500

    se crea la clase electodomestico, se le asignan los atributos y metodos señalados, metodos aun en blanco se crea la clase lavadora con los atributos y metodos señalados, metodos aun en blanco. se crea la clase television con los atributos y metodos señalados, metodos aun en blanco. clase electrodomestico padre de lavadora y television.

---
+++ src/electronicos/Lavadora.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package electronicos;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class Lavadora extends Electrodomestico{
+    /**
+     * atributo que contiene la carga de la lavadora
+     */
+    private  int carga;
+/**
+ * constructor con el precio y peso
+ * @param precio
+ * @param peso 
+ */
+    public Lavadora(double precio, double peso) {
+        super(precio, peso);
+    }
+/**
+ * constructor con carga peso y precio
+ * @param carga
+ * @param precio
+ * @param peso 
+ */
+    public Lavadora(int carga, double precio, double peso) {
+        super(precio, peso);
+        this.carga = carga;
+    }
+/**
+ * metodo que calcula el precio final de la lavadora
+ */
+    @Override
+    public void precioFinal(){
+        
+    }//precioFinal
+/**
+ * retorna el valor de la carga
+ * @return carga
+ */
+    public int getCarga() {
+        return carga;
+    }
+    
+    
+    
+}//Lavadora
